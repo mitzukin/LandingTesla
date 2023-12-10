5d642a1952b725b1566b58e8745f13b07eba9c9f
Jason Tajor <jasontajor0@gmail.com>
2023-12-11 00:27:31 +0800

    Initial Commit

--- Tesla-Sample/src/App.jsx
+++ Tesla-Sample/src/App.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./Components/Layout";
 import Home from "./Pages/Home";
+
 function App() {
   return (
-      <div>
-        <BrowserRouter basename="/home">
-          <Routes>
-            <Route path="/" element={<Layout />}>
+    <div>
+      <BrowserRouter basename="/LandingTesla">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            {/* Use "home" as the default route */}
             <Route index element={<Home />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </div>
-   
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
